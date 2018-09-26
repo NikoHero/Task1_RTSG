bdb96055420ac43f8d46c9981d60bc780c65718a
Nick <33632340+NikoHero@users.noreply.github.com>
2018-09-26 12:09:20 +0200

    Add files via upload

--- Task1/GADE1-17601321-Statheros/GADE1-17601321-Statheros/MeleeUnit.cs
+++ Task1/GADE1-17601321-Statheros/GADE1-17601321-Statheros/MeleeUnit.cs
@@ -8,7 +8,7 @@ namespace GADE1_17601321_Statheros
 {
     class MeleeUnit : Unit
     {
-
+        //Melee Class
 
 
         public MeleeUnit(int XPosition, int YPosition, int Health, int MaxHealth, int Speed,
